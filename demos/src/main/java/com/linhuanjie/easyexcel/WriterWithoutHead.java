5fd03978e49c0cf012b9d005ff3ced8da591ca35
linhuanjie <l>
2019-10-16 21:06:09 +0800

    commit

--- demos/src/main/java/com/linhuanjie/easyexcel/WriterWithoutHead.java
+++ demos/src/main/java/com/linhuanjie/easyexcel/WriterWithoutHead.java
@@ -9,7 +9,9 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * @author: linhuanjie
@@ -43,4 +45,41 @@ public class WriterWithoutHead {
             writer.finish();
         }
     }
+
+    public static void main(String[] args) {
+
+        try (OutputStream out = new FileOutputStream("清理20180101~20180331图片.xlsx")) {
+            ExcelWriter writer = new ExcelWriter(out, ExcelTypeEnum.XLSX, false);
+            Sheet sheet1 = new Sheet(1, 0);
+            sheet1.setSheetName("sheet1");
+            List<List<String>> data = new ArrayList<>();
+
+            String imgs = "20161114052736556.jpg,20161114052736556.jpg,20161114052747723.jpg,20161114052804260.jpg,20161114052815539.jpg";
+            String[] imgsArray = imgs.split(",");
+//            java7
+//            for (String s : imgsArray) {
+//                List<String> item = new ArrayList<>();
+//                item.add(s);
+//                data.add(item);
+//            }
+
+//            java8
+            Arrays.stream(imgsArray).distinct()
+                    .collect(Collectors.toList())
+                    .forEach(s -> data.add(new ArrayList<>(Arrays.asList(s))));
+//            Arrays.stream(split).distinct().collect(Collectors.toList()).forEach(s -> System.out.println(s));
+
+            data.stream().forEach(strings -> System.out.println(strings));
+
+            writer.write0(data, sheet1);
+            writer.finish();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+    }
+
+
+
 }
