d04ccf004cec479e9af90c7af0f03782e47e100a
linhj <linhj@mama.cn>
2020-03-25 18:32:45 +0800

    :pencil: commit  RestTemplate

--- demos/src/main/java/com/linhuanjie/spring/RestTemplateDemo.java
+++ demos/src/main/java/com/linhuanjie/spring/RestTemplateDemo.java
@@ -1,67 +1,26 @@
 package com.linhuanjie.spring;
 
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
 /**
+ * 详解 RestTemplate 操作   https://blog.csdn.net/xmtblog/article/details/82761642
+ * 用更优雅的方式发HTTP请求(RestTemplate详解)   https://www.cnblogs.com/javazhiyin/p/9851775.html
  * @author: linhuanjie
- * @description:   用更优雅的方式发HTTP请求(RestTemplate详解)（https://www.cnblogs.com/javazhiyin/p/9851775.html）
  * @createTime : 2018-11-29 10:07
  * @email: lhuanjie@qq.com
  */
 
 public class RestTemplateDemo {
 
-//    public static void main(String[] args) {
-//        RestTemplate restTemplate = new RestTemplate();
-//        String forObject = restTemplate.getForObject("ly.mama.cn/main/index", String.class);
-//        System.out.println("forObject = " + forObject);
-//
-//        ResponseEntity<String> forEntity = restTemplate.getForEntity("ly.mama.cn/main/index", String.class);
-//        System.out.println("forEntity = " + forEntity);
+    public static void main(String[] args) {
+        RestTemplate restTemplate = new RestTemplate();
+        String url = "http://pms.api.xiaoshuxiong.com/pmsapi/check/info/all";
+        String forObject = restTemplate.getForObject(url, String.class);
+        System.out.println("forObject = " + forObject);
 
-
-//    }
-    public static void main(String[] args)  {
-        String a = "hello2";
-        final String b = getHello();
-        String c = b + 2;
-        String d = "hello" + 2;
-        System.out.println((a == c));
-        System.out.println((a == d));
-        System.out.println((a));
-        System.out.println((b));
-        System.out.println((c));
-
-    }
-
-    public static String getHello() {
-        return "hello";
-    }
-
-}
-
-class Test {
-    public static void main(String[] args)  {
-//        MyClass myClass = new MyClass();
-//        StringBuffer buffer = new StringBuffer("hello");
-//        myClass.changeValue(buffer);
-//        System.out.println(buffer.toString());
-
-        String a = "111";
-        System.out.println("a = " + a);
-        System.out.println("a = " + test(a));
-        System.out.println("a = " + a);
-
-    }
-
-    public static String test(String a ){
-        a = a + "QAQ";
-        return a;
-    }
-}
-
-class MyClass {
-
-    void changeValue(final StringBuffer buffer) {
-        buffer.append("world");
+        ResponseEntity<String> forEntity = restTemplate.getForEntity(url, String.class);
+        System.out.println("forEntity = " + forEntity);
     }
 }
